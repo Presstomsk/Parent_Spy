076d77cf1d668cc12d3a5b56a78de0b92fc51407
Roman Permiakov <info@presstomsk.ru>
2022-10-13 22:16:39 +0700

    Windows Service

--- Parent_Spy/Program.cs
+++ Parent_Spy/Program.cs
@@ -17,10 +17,10 @@ namespace Parent_Spy
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args)                
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseWindowsService();
     }
 }
